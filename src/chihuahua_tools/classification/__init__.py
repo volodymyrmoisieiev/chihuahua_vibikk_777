6e8c87b4b98ec8130d4e01b90c5c2064bc4b1d98
volodymyrmoisieiev <vladimir20moiseev@gmail.com>
2025-12-01 04:48:06 +0200

    Update __init__.py

--- src/chihuahua_tools/classification/__init__.py
+++ src/chihuahua_tools/classification/__init__.py
@@ -5,6 +5,22 @@ Install extras:
     pip install -e ".[torch]"
 """
 
-from .predict import load_model, predict_path
+from .predict import (
+    load_model,
+    predict_image,
+    predict_path,
+    predict_path_with_plot,
+    predict_many,
+    ClassificationResult,
+    CLASS_NAMES,
+)
 
-__all__ = ["load_model", "predict_path"]
+__all__ = [
+    "load_model",
+    "predict_image",
+    "predict_path",
+    "predict_path_with_plot",
+    "predict_many",
+    "ClassificationResult",
+    "CLASS_NAMES",
+]
